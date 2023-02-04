6c382436c55dc4b2245dd82e5a39f715c1a9d395
Calvin <calvinchiamjh@gmail.com>
2023-02-04 21:12:31 +0800

    Add laser firing to plane

--- README.md
+++ README.md
@@ -7,12 +7,15 @@ This project is an ambitious studio project of creating a web game. The name of 
 This website is for those that wants a blast from the past of arcade game while being reinvented in our rendition of the game. This website is in hope of acheiving a retroactive arcade style of memory with this project heavily trying to recreate that experience.
 Wireframe: https://xd.adobe.com/view/47ae0e0b-9d3c-4af3-b017-182692ef05bc-12fd/
 
+The theme of the website is minimalism. Interactive elements built in the web to attract the user
+
 ## Features
 
 ## Existing Features
 
 - Cursor is a crosshair
 - background music
+- Fire projectiles
 
 ### Feature left to implement
 
@@ -24,12 +27,14 @@ Wireframe: https://xd.adobe.com/view/47ae0e0b-9d3c-4af3-b017-182692ef05bc-12fd/
 
 - [Adobe XD](https://www.adobe.com/sg/products/xd.html)
   - This project uses XD for wireframing of the website and mobile counterpart.
+- [Adobe Express](https://express.adobe.com/sp/)
+  - This project uses express for creation of the plane model
 - [HTML](https://html.com)
   - This project uses HTML for layout of the site content
 - [CSS](https://www.w3.org/TR/CSS/#css)
   - This project uses CSS for decoration of the site
 - [JavaScript](https://www.javascript.com)
-  - This project uses JS for automation of some features on the site
+  - This project uses JS for creation of the game
 
 ## Testing
 
@@ -45,3 +50,5 @@ when user clicks the background
 ### Media
 
 ### Acknowledgements
+
+- I received inspiration for this project from [YouTube] (https://www.youtube.com/@ChrisCourses)
--- game.js
+++ game.js
@@ -14,14 +14,14 @@ class Plane {
         this.rotation = 0
 
         const plane = new Image()
-        plane.src = './Images/fighter-jet.png'
+        plane.src = './Images/Fighter jet 2.png'
         plane.onload = () => {
         this.image = plane
-        this.width = plane.width * 0.5
-        this.height = plane.height * .5
+        this.width = plane.width * .2
+        this.height = plane.height * .2
         this.position = {
             x: canvas.width / 2 - this.width / 2, //Set x position of plane to be center of web 
-            y: canvas.height - this.height - 100 //Set y position of plane to be bottom of web
+            y: canvas.height - this.height - 20 //Set y position of plane to be bottom of web
         }           
         }
     }
@@ -33,7 +33,7 @@ class Plane {
             plane.position.y + plane.height / 2
         )
 
-        c.rotate(this.rotation)        
+        c.rotate(this.rotation) // Rotate image with canvas
 
         c.translate(
             -plane.position.x - plane.width / 2,
@@ -52,7 +52,32 @@ class Plane {
     }
 }
 
+class Laser {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 5
+    }
+    
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2) //Create a circle 
+        c.fillStyle = 'rgb(223, 15, 15)'
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
 const plane = new Plane()
+
+const lasers = [] // To fire multiple laser 
 
 const keys = {
     q: {
@@ -73,6 +98,18 @@ function animate() { // Initalize plane image
     c.fillStyle = '#333'
     c.fillRect(0, 0, canvas.width, canvas.height)
     plane.update()
+    lasers.forEach((Laser, index) => {
+        if (Laser.position.y + Laser.radius <= 0) { //Remove fired laser from array to prevent it slowing down process
+            setTimeout(() => { // Prevent afterimage of laser bullet
+                lasers.splice(index, 1)                
+            }, 0)
+        }
+
+        else {
+            Laser.update() //Update position of each laser bullet            
+        }
+
+    })
     if (keys.q.pressed && plane.position.x >= 0) {
         plane.velocity.x = -10
         plane.rotation = -0.15
@@ -92,31 +129,37 @@ animate()
 addEventListener('keydown', ({key}) => { //Get the input being pressed by the player
     switch (key) {
         case 'q': // Move plane to the left
-            console.log ('left')
             keys.q.pressed = true
             break
         case 'e': // Move plane to the right
-            console.log ('right')
             keys.e.pressed = true
             break
         case ' ': // Fire button
-            console.log ('space')
+            lasers.push(new Laser({ // Append laser to array 
+                position: {
+                    x: plane.position.x + plane.width / 2,
+                    y: plane.position.y,
+            
+                },
+            
+                velocity: {
+                    x:0,
+                    y:-20
+                }
+            }))
             break
     }
 })
 
-addEventListener('keyup', ({key}) => { //Get the input being pressed by the player
+addEventListener('keyup', ({key}) => { //Get the key being released by the player
     switch (key) {
         case 'q': // Stop plane movement
-            console.log ('left')
             keys.q.pressed = false
             break
         case 'e':
-            console.log ('right')
             keys.e.pressed = false
             break
-        case ' ': // Fire button
-            console.log ('space')
-            break
+        // case ' ': // Fire button
+        //     break
     }
 })
